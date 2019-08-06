91f1a6a3cbe8d82400c0a86450f1a38d800d7479
kirbycampbell <jkirbycampbell@gmail.com>
2019-08-06 09:40:43 -0700

    Update cors

--- server/index.js
+++ server/index.js
@@ -6,7 +6,19 @@ require("dotenv").config({ path: "./.env" });
 const app = express();
 
 //Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://www.elecenter.com/"
+  })
+);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://www.elecenter.com/"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(bodyParser.json());
 
 const email = require("./routes/api/email");
